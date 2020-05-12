fa926836d1b91fb87f31c6388bf2d50615d25140
CreedsCode <hoffmann.dercio@hotmail.com>
2020-05-13 00:24:30 +0200

    init

---
+++ mooDGUesSEr.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+using MooDGUesSErML.Model;
+
+namespace mooDGUesSEr.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _enginePool;
+
+        public IndexModel(ILogger<IndexModel> logger, PredictionEnginePool<ModelInput, ModelOutput> enginePool )
+        {
+            _logger = logger;
+            _enginePool = enginePool;
+        }
+
+
+        public IActionResult OnGetMood([FromQuery]string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Clear");
+            var input = new ModelInput { SentimentText = text };
+            var prediction = _enginePool.Predict(input);
+            var result = Convert.ToBoolean(prediction.Prediction) ? "Bad" : "Good";
+            return Content(result);
+        }
+
+
+        public void OnGet(){}
+
+    }
+}
---
+++ mooDGUesSEr.Web/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ML;
+using MooDGUesSErML.Model;
+
+namespace mooDGUesSEr.Web
+{
+    public class Startup
+    {
+        private readonly string _modelPath;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            var fileInfo = new FileInfo(typeof(Program).Assembly.Location);
+            var assemblyPath = fileInfo.Directory.FullName;
+            _modelPath = Path.Combine(assemblyPath, "MLModel.zip");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddPredictionEnginePool<ModelInput, ModelOutput>().FromFile(_modelPath);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
